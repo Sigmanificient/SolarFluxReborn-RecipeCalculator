424cbe76378736569b98da084a116c654683ba4d
Yohann Boniface <edhyjox@gmail.com>
2021-08-06 22:13:54 +0200

    cli choose_from return choice

--- src/cli.py
+++ src/cli.py
@@ -2,17 +2,15 @@ from PyInquirer import prompt
 
 
 def choose_from(question, *choices):
-    """Let the user choose between a list of options. return index."""
-    return choices.index(
-        prompt(
-            {
-                "type": "list",
-                "name": "command",
-                "message": question,
-                "choices": choices
-            }
-        ).get("command")
-    )
+    """Let the user choose between a list of options."""
+    return prompt(
+        {
+            "type": "list",
+            "name": "command",
+            "message": question,
+            "choices": choices
+        }
+    ).get("command")
 
 
 def ask(question):
