f6bb3a1aa75474dba7019efbb29b6432e723a3ef
Yohann Boniface <edhyjox@gmail.com>
2021-08-06 23:02:12 +0200

    adding amount question

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+from typing import Optional, List, Dict
+
 from src import cli, solar_panels
+from src.classes.item import Item
 from src.utils.counter import recursive_list_count
 
 
-def main():
-    action = None
+def main() -> None:
+    action: Optional[str] = None
 
     while action != 'quit':
-        tier = cli.choose_from('tier', *map(str, range(1, 7)))
+        tier: str = cli.choose_from('tier', *map(str, range(1, 7)))
 
-        amount = ''
+        amount: str = ''
         while not amount.isdigit():
             amount = cli.ask('Choose an amount [1]') or '1'
 
-        amount = int(amount)
+        int_amount: int = int(amount)
 
-        to_craft = solar_panels[int(tier) - 1].get_raw_recipe() * amount
-        count = recursive_list_count(to_craft)
+        to_craft: List[Item] = (
+            solar_panels[int(tier) - 1].get_raw_recipe() * int_amount
+        )
 
-        print("Total ressources needed: ")
-        print('\n'.join(f"- {k} x{v}" for k, v in sorted(count.items())))
-        print()
+        count: Dict[Item, int] = recursive_list_count(to_craft)
 
-        action = cli.choose_from('action', 'get materials', 'quit')
+        cli.pretty_list(
+            "Total ressources needed",
+            (f"{k} x{v:,}" for k, v in sorted(count.items()))
+        )
+
+        action: str = cli.choose_from('action', 'get materials', 'quit')
 
 
 if __name__ == '__main__':
