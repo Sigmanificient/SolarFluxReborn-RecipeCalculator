9cd7900ffc397c98f977e12dca896ae212f88a55
Yohann Boniface <edhyjox@gmail.com>
2021-08-06 22:55:00 +0200

    adding amount question

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ def main():
     while action != 'quit':
         tier = cli.choose_from('tier', *map(str, range(1, 7)))
 
-        to_craft = solar_panels[int(tier) - 1].get_raw_recipe()
+        amount = ''
+        while not amount.isdigit():
+            amount = cli.ask('Choose an amount [1]') or '1'
+
+        amount = int(amount)
+
+        to_craft = solar_panels[int(tier) - 1].get_raw_recipe() * amount
         count = recursive_list_count(to_craft)
 
         print("Total ressources needed: ")
         print('\n'.join(f"- {k} x{v}" for k, v in sorted(count.items())))
+        print()
 
         action = cli.choose_from('action', 'get materials', 'quit')
 
